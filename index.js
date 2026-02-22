61f67c1306433e392212fe2479c140868b80d60f
boansu <hank800222@gmail.com>
2026-02-22 13:44:43 +0800

    add health check

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const config = {
 const client = new line.Client(config);
 const app = express();
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.post('/webhook', line.middleware(config), (req, res) => {
   res.status(200).end();
   Promise.all(req.body.events.map(handleEvent)).catch(console.error);
